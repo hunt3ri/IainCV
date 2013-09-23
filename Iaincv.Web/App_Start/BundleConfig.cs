bb6cbd2a9aba3e6fd018324ea1676a2d5ed3d739
Iain Hunter <iain.hunter@outlook.com>
2013-09-23 23:10:24 +0100

    Inital work on bundles and bootstrap

---
+++ Iaincv.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Iaincv.Web.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+        }
+    }
+}
--- Iaincv.Web/Global.asax.cs
+++ Iaincv.Web/Global.asax.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Iaincv.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Iaincv.Web
@@ -19,6 +21,7 @@ namespace Iaincv.Web
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
