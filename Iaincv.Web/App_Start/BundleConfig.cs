e7e067b1117e8f4a83b22598458512be9658dedf
Iain Hunter <iain.hunter@outlook.com>
2013-09-23 23:34:19 +0100

    Get bootstrap working

--- Iaincv.Web/App_Start/BundleConfig.cs
+++ Iaincv.Web/App_Start/BundleConfig.cs
@@ -11,10 +11,21 @@ namespace Iaincv.Web.App_Start
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
+            // Stylesheets
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap/bootstrap.css"));
+
+            // Scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-1.*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            
+
+
+
+            
         }
     }
 }
